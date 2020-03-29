1573a0b32fad07df281fbc8aebe47ad0273e25bb
Valentin Raduti <valentin@thedotin.ro>
2020-03-29 12:58:56 +0300

    added a short selector

--- devtools.js
+++ devtools.js
@@ -5,6 +5,11 @@ function getDomPath(el) {
         return el.nodeName.toLowerCase() + (el.shadowRoot ? '::shadowRoot' : '');
     }
 
+    function findByText(selector, text) {
+        let els = document.querySelectorAll(selector);
+        return els.filter(el => el.children.length === 0 && el.nodeName.toLowerCase() !== 'style' && el.innerText && el.innerText === text);
+    }
+
     function recurse(el) {
         var stack = [];
         while (el.parentNode) {
@@ -25,10 +30,10 @@ function getDomPath(el) {
                 stack.unshift(name(el) + '[title=' + el.id + ']');
             } else if (el.children.length === 0 && el.nodeName.toLowerCase() !== 'style' && el.innerText && el.innerText !== '') {
                 stack.unshift(name(el) + ':contains(' + el.innerText + ')')
+            } else if (sibCount > 1) {
+                stack.unshift('> ' + name(el) + ':eq(' + sibIndex + ')');
             } else if (el.hasAttribute('class') && el.className !== '') {
                 stack.unshift(name(el) + '.' + el.className.split(' ').join('.'));
-            } else if (sibCount > 1) {
-                stack.unshift(name(el) + ':eq(' + sibIndex + ')');
             } else {
                 stack.unshift(name(el));
             }
@@ -54,24 +59,37 @@ function getDomPath(el) {
 
 chrome.devtools.panels.elements.createSidebarPane("FormulaDB",
     function (sidebar) {
+
+        let containsRegex = /^.*\:contains\((.+?)\)$/i;
+
         function getSelector(raw) {
-            var fstack = [];
+            let fstack = [];
+            let shortStack = [];
             if (raw && raw.length > 0) {
                 if (raw[0].startsWith('iframe')) {
                     fstack.push(raw[0]);
                 }
-                var foundId = false;
+                let foundId = false;
                 for (var i = raw.length - 1; i > 1; i--) {
                     if (raw[i].indexOf('#') > -1) {
-                        fstack = [...fstack, ...raw.slice(0,i).filter(r => r.indexOf('::shadowRoot')>-1), ...raw.slice(i)];
-                        console.log(fstack);
+                        fstack = [...fstack, ...raw.slice(0, i).filter(r => r.indexOf('::shadowRoot')), ...raw.slice(i)];
                         foundId = true;
-                        break;
                     }
                 }
                 if (!foundId) fstack = raw;
+                shortStack = fstack;
+                let matchContains = fstack[fstack.length - 1].match(containsRegex);
+                if (matchContains !== null && matchContains.length > 1) {
+                    shortStack = fstack.slice(0, fstack.length - 2)
+                        .filter(s => s.indexOf('iframe') > -1 || s.indexOf('::shadowRoot') > -1 || s.indexOf('#') > -1 || s.indexOf('[title') > -1);
+                    // let selector = shortStack.join(' ');
+                    // let containsEls = findByText(selector, matchContains[1]);
+                    // if (containsEls.length === 1) {
+                    // }
+                    shortStack = [...shortStack, fstack[fstack.length-1]];
+                }
             }
-            sidebar.setObject({ stack: raw, selector: fstack.join(' ') });
+            sidebar.setObject({ stack: raw, selector: fstack.join(' '), short: shortStack.join(' ') });
         }
 
         sidebar.setObject({ selector: "Please select an item" });
